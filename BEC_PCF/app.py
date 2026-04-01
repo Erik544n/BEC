eb518c9b023373719ade4528c22190fe33bfc7ff
ErikNB <123046529@upq.edu.mx>
2026-03-31 23:30:17 -0600

    otro

--- BEC_PCF/app.py
+++ BEC_PCF/app.py
@@ -109,6 +109,6 @@ def ciudadano_voluntariados():
     return render_template('ciudadano_voluntariados.html')
 
 if __name__ == '__main__':
- 
+
     puerto = int(os.getenv('PORT', 8080))
     app.run(debug=True, host='0.0.0.0', port=puerto)
